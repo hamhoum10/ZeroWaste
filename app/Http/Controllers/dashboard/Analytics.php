0fbe40ff10b1b205588bece066507fb9475aee7a
yassineghanmi <yassine.ghanmi@esen.tn>
2024-10-10 17:51:50 +0100

    last update

--- app/Http/Controllers/dashboard/Analytics.php
+++ app/Http/Controllers/dashboard/Analytics.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
 
 class Analytics extends Controller
 {
@@ -15,8 +15,15 @@ class Analytics extends Controller
   {
     $totalUsers = User::count();
     $totalOrders = Order::count();
-    $totalRevenue = Order::sum('total_price'); // Assuming 'total' is a column in the Order model
+    $totalRevenue = Order::sum('total_price');
     $totalProducts = Product::count();
-    return view('content.dashboard.dashboards-analytics',compact('totalUsers','totalOrders','totalRevenue','totalProducts'));
+
+    $today = Carbon::today();
+    $latestOrders = Order::whereDate('created_at', $today) // Filter orders by today's date
+    ->orderBy('created_at', 'desc') // Order by the latest created timestamp
+    ->take(5) // Limit the results to 5
+    ->get();
+
+    return view('content.dashboard.dashboards-analytics', compact('totalUsers', 'totalOrders', 'totalRevenue', 'totalProducts', 'latestOrders'));
   }
 }
