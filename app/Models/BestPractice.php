ca5b40644e3c3ee9ad0a359ca8ac33a08a3009cd
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-11-01 19:00:17 +0100

    Added Edit And Delete In Comment Section

--- app/Models/BestPractice.php
+++ app/Models/BestPractice.php
@@ -19,4 +19,10 @@ class BestPractice extends Model
   public function author() {
     return $this->belongsTo(User::class, 'author_id');
   }
+
+  public function comments()
+  {
+    return $this->hasMany(Comment::class);
+  }
+
 }
